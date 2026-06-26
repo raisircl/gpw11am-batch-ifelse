66f274dc3f045846d165e27c8957597302b762f0
raisircl <rai.verma@live.com>
2026-06-26 13:08:47 +0530

    if elif else

---
+++ dayofweek.py
@@ -0,0 +1,20 @@
+day=int(input("Enter day number (1-7): "))
+if day==1:
+    print("Monday")
+elif day==2:
+    print("Tuesday")
+elif day==3:
+    print("Wednesday")
+elif day==4:
+    print("Thursday")
+elif day==5:
+    print("Friday")
+elif day==6:
+    print("Saturday")
+elif day==7:
+    print("Sunday")
+else:
+    print("Invalid day number. Day should be between 1-7.")
+
+#enter a digit and print it in word
+# 1 One, 2 Two, 3 Three, 4 Four, 5 Five, 6 Six, 7 Seven, 8 Eight, 9 Nine, 0 Zero
